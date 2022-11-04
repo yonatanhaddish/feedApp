d72a53de737bc12c2d696d8a680f41a5587cbe6a
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-03 20:49:49 -0400

    comment added into ImageMetaData.java

--- src/main/java/com/bptn/model/ImageMetaData.java
+++ src/main/java/com/bptn/model/ImageMetaData.java
@@ -31,12 +31,12 @@ public class ImageMetaData {
 	@Column(name = "\"postKey\"")
 	private String postKey;
 	
-	
+//	constructor
 	public ImageMetaData() {
 		
 		super();
 	}
-	
+//	constructor with parameters
 	public ImageMetaData(String imageID, String imageName, String imageSize, String imageFormat, String imageDate, String resolution, String postKey) {
 		
 		super();
@@ -49,59 +49,59 @@ public class ImageMetaData {
 		this.resolution = resolution;
 		this.postKey = postKey;
 	}
-
+// imageID getter method
 	public String getImageID() {
 		return this.imageID;
 	}
-
+//imageID setter method
 	public void setImageID(String imageID) {
 		this.imageID = imageID;
 	}
-
+//imageName getter method
 	public String getImageName() {
 		return this.imageName;
 	}
-
+//imageName setter method
 	public void setImageName(String imageName) {
 		this.imageName = imageName;
 	}
-
+//imageSize getter method
 	public String getImageSize() {
 		return this.imageSize;
 	}
-
+//imageSize setter method
 	public void setImageSize(String imageSize) {
 		this.imageSize = imageSize;
 	}
-
+//imageFormat getter method
 	public String getImageFormat() {
 		return this.imageFormat;
 	}
-
+//imageFormat setter method
 	public void setImageFormat(String imageFormat) {
 		this.imageFormat = imageFormat;
 	}
-
+//imageDate getter method
 	public String getImageDate() {
 		return this.imageDate;
 	}
-
+//imageDate setter method
 	public void setImageDate(String imageDate) {
 		this.imageDate = imageDate;
 	}
-
+//resolution getter method
 	public String getResolution() {
 		return this.resolution;
 	}
-
+//resolution setter method
 	public void setResolution(String resolution) {
 		this.resolution = resolution;
 	}
-
+//postKey getter method
 	public String getPostKey() {
 		return this.postKey;
 	}
-
+//postKey setter method
 	public void setPostKey(String postKey) {
 		this.postKey = postKey;
 	}
