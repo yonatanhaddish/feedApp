feb87b0a21bc9cdd60e0f6be5aa8feb8723ea637
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-10 14:20:10 -0500

    FeedHistoryRepository created

---
+++ src/main/java/com/bptn/repository/FeedHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.bptn.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bptn.model.History;
+
+@Repository
+public interface FeedHistoryRepository extends JpaRepository<History, String> {
+	
+	List<History> findByUsernameKey(String usernameKey);
+	
+	History findByPostID(String postID);
+	
+	History findByPostType(String postType);
+	
+	void deleteByPostType(String postType);
+
+}
