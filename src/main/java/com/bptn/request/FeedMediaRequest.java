c5b03fd9129131bac4b898023e5ea6e9a31a922e
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-10 09:20:46 -0500

    FeedMediaPostRequest created

---
+++ src/main/java/com/bptn/request/FeedMediaRequest.java
@@ -0,0 +1,45 @@
+package com.bptn.request;
+
+public class FeedMediaRequest {
+	
+	private String imageName;
+	private String imageSize;
+	private String imageFormat;
+	private String resolution;
+	private String postKey;
+	
+	
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getImageSize() {
+		return imageSize;
+	}
+	public void setImageSize(String imageSize) {
+		this.imageSize = imageSize;
+	}
+	public String getImageFormat() {
+		return imageFormat;
+	}
+	public void setImageFormat(String imageFormat) {
+		this.imageFormat = imageFormat;
+	}
+	public String getResolution() {
+		return resolution;
+	}
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	public String getPostKey() {
+		return postKey;
+	}
+	public void setPostKey(String postKey) {
+		this.postKey = postKey;
+	}
+	
+	
+
+}
