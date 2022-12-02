980ed60250d658f708eb5a54593e5d386dc9363e
Yonatan Haddish <yonihailshn@gmail.com>
2022-12-02 12:01:22 -0500

    added jjwt auth

--- src/main/java/com/bptn/controller/LoginController.java
+++ src/main/java/com/bptn/controller/LoginController.java
@@ -1,3 +1,52 @@
+package com.bptn.controller;
+
+import com.bptn.exceptions.InvalidRequestException;
+import com.bptn.exceptions.InvalidUserCredentialsException;
+import com.bptn.model.UserID;
+import com.bptn.request.LoginRequest;
+import com.bptn.response.JwtResponse;
+import com.bptn.response.LoginResponse;
+import com.bptn.service.JwtService;
+import com.bptn.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class LoginController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private LoginService loginService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    /*
+        {
+        "username": "jDoe3286",
+        "password":"1960"
+        }
+    */
+    @PostMapping("/user/login")
+    public ResponseEntity<?> verifyUserCredentials(@RequestBody LoginRequest loginRequest) throws InvalidUserCredentialsException {
+        LOGGER.debug("Login request received for username = {} & password = {}", loginRequest.getUsername(), loginRequest.getPassword());
+        UserID userID = loginService.verifyUserCredentials(loginRequest);
+        if (userID == null) {
+            throw new InvalidUserCredentialsException("Invalid User");
+        }
+        JwtResponse jwtResponse = new JwtResponse(jwtService.generateJwtToken(loginRequest.getUsername()));
+        return new ResponseEntity<>(new LoginResponse(userID, jwtResponse), HttpStatus.OK);
+    }
+}
+
 //package com.bptn.controller;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/bptn/service/LoginService.java
+++ src/main/java/com/bptn/service/LoginService.java
@@ -1,3 +1,42 @@
+package com.bptn.service;
+
+
+import com.bptn.exceptions.InvalidUserCredentialsException;
+import com.bptn.model.AuthenticationUser;
+import com.bptn.model.UserID;
+import com.bptn.repository.AuthenticationUserRepository;
+import com.bptn.request.LoginRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private AuthenticationUserRepository authenticationUserRepository;
+
+    public UserID verifyUserCredentials(LoginRequest loginRequest) throws InvalidUserCredentialsException {
+        LOGGER.debug("Invoking login = {}", "verifyUserCredentials");
+        if (StringUtils.isEmpty(loginRequest.getUsername()) || StringUtils.isEmpty(loginRequest.getPassword())){
+            LOGGER.debug("Username = {} or Password = {} can't be empty", loginRequest.getUsername(), loginRequest.getPassword());
+            throw new InvalidUserCredentialsException("Username or Password can't be empty");
+        }
+        AuthenticationUser authenticationUser =
+                authenticationUserRepository.findAuthenticationUserByIdAndUserPassword(loginRequest.getUsername(), loginRequest.getPassword());
+        if (authenticationUser == null) {
+            throw new InvalidUserCredentialsException("Invalid Credentials");
+        }
+        LOGGER.debug("Username = {} or Password = {} validated successfully", loginRequest.getUsername(), loginRequest.getPassword());
+        return authenticationUser.getUserID();
+    }
+
+}
+
 //package com.bptn.service;
 //
 //import java.util.List;
